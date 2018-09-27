2eb46c0f150ce813c001ce8a746e9f7f9007b1e5
Joschka Schwarz <j.schwarz@t5a-nbk-012.tie.tu-harburg.de>
2018-09-27 11:37:48 +0200

    Insert template

--- Chapter 03: Data visualisation/ggplot.R
+++ Chapter 03: Data visualisation/ggplot.R
@@ -2,8 +2,12 @@
 library(tidyverse)
 
 # Creating a ggplot -------------------------------------------------------
-#"displ" on the x-axis and "hwy" on the y-axis
-ggplot(data = mpg) + 
-  geom_point(mapping = aes(x = displ, y = hwy))
+# TEMPLATE
+# ggplot(data = <DATA>) +                               // creates coordinate system
+#   <GEOM_FUNCTION>(mapping = aes(<MAPPINGS>))          // adds layer (e.g. scatterplot)
+
+# EXAMPLE ("displ" on the x-axis and "hwy" on the y-axis)
+ggplot(data = mpg) +                              
+  geom_point(mapping = aes(x = displ, y = hwy))   
 
 
